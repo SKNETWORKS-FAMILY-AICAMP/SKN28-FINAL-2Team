1bc3825d918c87daf51100934abc22cf088f7203
jyyyyy4 <annajy1425@gmail.com>
2026-07-28 16:05:58 +0900

    backend 불필요 파일 삭제

--- frontend/src/backend/apps/bookmark/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import BookmarkDetailAPIView, BookmarkListCreateAPIView
-
-urlpatterns = [
-    path("", BookmarkListCreateAPIView.as_view(), name="bookmark-list-create"),
-    path("<int:pk>/", BookmarkDetailAPIView.as_view(), name="bookmark-detail"),
-]
