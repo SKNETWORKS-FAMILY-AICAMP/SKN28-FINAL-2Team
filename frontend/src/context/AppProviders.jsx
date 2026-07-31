97961d6e493e60ebffe93d360e95fee25aab94ff
jyyyyy4 <annajy1425@gmail.com>
2026-07-31 09:47:38 +0900

    장바구니 feature/frontend 병합

--- frontend/src/context/AppProviders.jsx
+++ frontend/src/context/AppProviders.jsx
@@ -2,13 +2,20 @@ import { AuthProvider } from './AuthContext.jsx'
 import { BookmarkProvider } from './BookmarkContext.jsx'
 import { ReservationProvider } from './ReservationContext.jsx'
 import { ItineraryProvider } from './ItineraryContext.jsx'
+import { CartProvider } from './CartContext.jsx'
+import CartWidget from '../components/CartWidget.jsx'
 
 export default function AppProviders({ children }) {
   return (
     <AuthProvider>
       <BookmarkProvider>
         <ReservationProvider>
-          <ItineraryProvider>{children}</ItineraryProvider>
+          <ItineraryProvider>
+            <CartProvider>
+              {children}
+              <CartWidget />
+            </CartProvider>
+          </ItineraryProvider>
         </ReservationProvider>
       </BookmarkProvider>
     </AuthProvider>
