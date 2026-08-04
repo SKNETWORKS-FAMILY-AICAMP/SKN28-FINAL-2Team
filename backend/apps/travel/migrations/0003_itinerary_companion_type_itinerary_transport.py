1f45cee885840bcc4c7a4f35bcac3f0dfdfc2ddc
jyyyyy4 <annajy1425@gmail.com>
2026-08-04 14:07:47 +0900

    merge migrations

--- backend/apps/travel/migrations/0003_itinerary_companion_type_itinerary_transport.py
+++ backend/apps/travel/migrations/0003_itinerary_companion_type_itinerary_transport.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('travel', '0002_place_alter_package_options_touristspot_source_id_and_more'),
+        ('travel', '0002_place_touristspot_source_id_alter_itinerary_user' ),
     ]
 
     operations = [
